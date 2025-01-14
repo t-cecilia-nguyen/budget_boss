6114a628a34f86f70d30461d88dbb8606356d062
Cecilia Nguyen <cecilia.t.nguyen@gmail.com>
2025-01-13 20:22:03 -0500

    changed color palette

--- assets/colors.js
+++ assets/colors.js
@@ -1,8 +1,8 @@
 export const Colors = {
-    primaryBlue: "#1C5370", // Navigation background
-    accentYellow: "#FFBD2E", // Accent yellow color
-    lightGrey: "#B0BEC5", // Light grey color - navigation icons
-    green: "#34B550", // success, income
-    red: "#D96565", // error, expense
-    lightBlue: "#679AFF" // budget
+    primaryBlue: "#277DA1", // Navigation background
+    accentYellow: "#F9C74F", // Accent yellow color
+    greyBlue: "#577590", // Light grey color - navigation icons
+    green: "#90BE6D", // success, income
+    red: "#F94144", // error, expense
+    lightBlue: "#73BFDC" // budget
 }
