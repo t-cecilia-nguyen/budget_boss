893bfc5efeb95e6b6676a65c5c37601a665e8a3a
Cecilia Nguyen <cecilia.t.nguyen@gmail.com>
2025-01-16 18:06:25 -0500

    added Add button with tab nav

--- navigation/BottomTabNavigator.js
+++ navigation/BottomTabNavigator.js
@@ -1,6 +1,8 @@
+import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { MaterialIcons } from '@expo/vector-icons';
 import { Colors } from '../assets/colors';
+import { TouchableOpacity, View, StyleSheet } from 'react-native';
 
 // Import tabs
 import Transactions from '../screens/Transactions';
@@ -13,71 +15,89 @@ const Tab = createBottomTabNavigator();
 
 export default function BottomTabNavigator() {
     return (
-        <Tab.Navigator 
-        initialRouteName="Transactions" 
-        screenOptions={{ 
-            headerShown: false, 
-            tabBarActiveTintColor: Colors.accentYellow, 
-            tabBarInactiveTintColor: Colors.lightGrey }}>
-            <Tab.Screen 
-            name="Transactions" 
-            component={Transactions}
-            options={{
-                tabBarIcon: ({focused}) => (
-                    <MaterialIcons 
-                    name="view-list" 
-                    size={28} 
-                    color={focused ? Colors.accentYellow : Colors.lightGrey} />
-                ),
-            }} />
-            <Tab.Screen 
-            name="Overview" 
-            component={Overview} 
-            options={{
-                tabBarIcon: ({focused}) => (
-                    <MaterialIcons 
-                    name="pie-chart" 
-                    size={28} 
-                    color={focused ? Colors.accentYellow : Colors.lightGrey} />
-                ),
-            }}
-            />
-            <Tab.Screen 
-            name="Budgets" 
-            component={Budgets} 
-            options={{
-                tabBarIcon: ({focused}) => (
-                    <MaterialIcons 
-                    name="currency-exchange" 
-                    size={28} 
-                    color={focused ? Colors.accentYellow : Colors.lightGrey} />
-                ),
-            }}
-            />
-            <Tab.Screen 
-            name="Categories" 
-            component={Categories} 
-            options={{
-                tabBarIcon: ({focused}) => (
-                    <MaterialIcons 
-                    name="category" 
-                    size={30} 
-                    color={focused ? Colors.accentYellow : Colors.lightGrey} />
-                ),
-            }}
-            />
-            <Tab.Screen 
-            name="Reports" 
-            component={Reports} 
-            options={{
-                tabBarIcon: ({focused}) => (
-                    <MaterialIcons 
-                    name="stacked-line-chart" 
-                    size={28} 
-                    color={focused ? Colors.accentYellow : Colors.lightGrey} />
-                ),
-            }}
-            />
-        </Tab.Navigator>
+        <View style={{ flex: 1 }}>
+            <Tab.Navigator 
+                initialRouteName="Transactions" 
+                screenOptions={{ 
+                    headerShown: false, 
+                    tabBarActiveTintColor: Colors.accentYellow, 
+                    tabBarInactiveTintColor: Colors.lightGrey 
+                }}>
+                <Tab.Screen 
+                    name="Transactions" 
+                    component={Transactions}
+                    options={{
+                        tabBarIcon: ({focused}) => (
+                            <MaterialIcons 
+                                name="view-list" 
+                                size={28} 
+                                color={focused ? Colors.accentYellow : Colors.lightGrey} />
+                        ),
+                    }} />
+                <Tab.Screen 
+                    name="Overview" 
+                    component={Overview} 
+                    options={{
+                        tabBarIcon: ({focused}) => (
+                            <MaterialIcons 
+                                name="pie-chart" 
+                                size={28} 
+                                color={focused ? Colors.accentYellow : Colors.lightGrey} />
+                        ),
+                    }} />
+                <Tab.Screen 
+                    name="Budgets" 
+                    component={Budgets} 
+                    options={{
+                        tabBarIcon: ({focused}) => (
+                            <MaterialIcons 
+                                name="currency-exchange" 
+                                size={28} 
+                                color={focused ? Colors.accentYellow : Colors.lightGrey} />
+                        ),
+                    }} />
+                <Tab.Screen 
+                    name="Categories" 
+                    component={Categories} 
+                    options={{
+                        tabBarIcon: ({focused}) => (
+                            <MaterialIcons 
+                                name="category" 
+                                size={30} 
+                                color={focused ? Colors.accentYellow : Colors.lightGrey} />
+                        ),
+                    }} />
+                <Tab.Screen 
+                    name="Reports" 
+                    component={Reports} 
+                    options={{
+                        tabBarIcon: ({focused}) => (
+                            <MaterialIcons 
+                                name="stacked-line-chart" 
+                                size={28} 
+                                color={focused ? Colors.accentYellow : Colors.lightGrey} />
+                        ),
+                    }} />
+            </Tab.Navigator>
+            {/* Add button */}
+            <TouchableOpacity style={styles.floatingButton}>
+                <MaterialIcons name="add" size={30} color={Colors.white} />
+            </TouchableOpacity>
+        </View>
     );
 }
+
+const styles = StyleSheet.create({
+    floatingButton: {
+        position: 'absolute',
+        right: 10,
+        bottom: 60,
+        backgroundColor: Colors.accentYellow,
+        width: 50,
+        height: 50,
+        borderRadius: '50%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        elevation: 5,
+    },
+});
