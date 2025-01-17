4245d34cc274c5eb41f48dcaf31d0b77148022d1
Nhan T <nktran4@gmail.com>
2025-01-17 14:16:07 -0500

    backend server added

---
+++ Backend/database.py
@@ -0,0 +1,15 @@
+import sqlite3
+from flask import g
+
+DATABASE = './backend/database.db'
+
+def get_db():
+    if 'db' not in g:
+        g.db = sqlite3.connect(DATABASE)
+        g.db.row_factory = sqlite3.Row  
+    return g.db
+
+def close_db(e=None):
+    db = g.pop('db', None)
+    if db is not None:
+        db.close()
