7c248fedaa3573445b8ac0bff25ac38846dff06a
nhunique <lyhuynhyennhu01@gmail.com>
2025-01-26 13:43:46 -0500

    add password_hash to schema

--- Backend/schema/schema.sql
+++ Backend/schema/schema.sql
@@ -5,5 +5,5 @@ CREATE TABLE IF NOT EXISTS users (
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL,
     email TEXT NOT NULL UNIQUE,
-    password TEXT NOT NULL
+   password_hash TEXT NOT NULL
 );
